4e741782e862bf00c9474145a823e5a563443bc1
Shirisha Boori <118332167+shirishaaa17@users.noreply.github.com>
2024-10-05 13:45:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         setTimeout(() => {
                             entry.target.classList.add("show-text");
                         }, 500); // 500ms to wait for the button expansion
-                    }, 2000);  // 2-second delay after the bounce animation
+                    }, 1000);  // 2-second delay after the bounce animation
                 }
                 
                 observer.unobserve(entry.target); 
